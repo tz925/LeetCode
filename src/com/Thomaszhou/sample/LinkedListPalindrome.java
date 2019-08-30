fb8626111517aa45b3b1272ec2922d5a7607fc3d
tz925 <sai950925@hotmail.com>
2019-08-30 17:56:41 -0400

    LinkedListPalindrome

---
+++ src/com/Thomaszhou/sample/LinkedListPalindrome.java
@@ -0,0 +1,44 @@
+package com.Thomaszhou.sample;
+
+public class LinkedListPalindrome {
+    private static ListNode reverse(ListNode head) {
+        if (head == null){
+            return head;
+        }
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode next = cur.next;
+        while(cur!=null){
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        if(head == null) {
+            return true;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prevSlow = slow;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            prevSlow = slow;
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+
+        while(head != null && slow != null) {
+            if(head.val != slow.val) {
+                return false;
+            }
+            head = head.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
