1b987688a94a7430a3b7e6c897f920b724f3cae2
tz925 <sai950925@hotmail.com>
2019-09-01 14:01:50 -0400

    FloodFill

--- src/com/Thomaszhou/sample/Main.java
+++ src/com/Thomaszhou/sample/Main.java
@@ -12,5 +12,19 @@ public class Main {
         System.out.println(s);
         s = s.replaceAll("/s","");
         System.out.println(s);
+        int a = 5;
+        int b = a;
+        b = 10;
+        System.out.println(a);
+        System.out.println(b);
+        int[][] image = new int[][]{{1,1,1},{1,1,0},{1,0,1}};
+        image = FloodFill.floodFill(image, 1,1, 2);
+        for(int[] i : image){
+            for(int j : i){
+                System.out.println(j);
+            }
+        }
+
+
     }
 }
