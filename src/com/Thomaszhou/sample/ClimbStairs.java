99ff69be9a1d064ccaf0ef9a8a6cc3b127ef47e1
tz925 <sai950925@hotmail.com>
2019-09-04 16:20:21 -0400

    Climb Stair - Basically Fib # if you know how to analyze

---
+++ src/com/Thomaszhou/sample/ClimbStairs.java
@@ -0,0 +1,21 @@
+package com.Thomaszhou.sample;
+
+public class ClimbStairs {
+    public int climbStairs(int n) {
+        // total ways = #ways if we take 1 step + #ways if we take 2 steps
+        // T(n) = T(n - 1) + T(n - 2)
+        // basically Fibonacci Numbers. find out base case value and we can get answer.
+        // Time(n) Space(1)
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        int p1 = 1;
+        int p2 = 2;
+        int ans = 0;
+        for(int i = 3; i <=n; i++){
+            ans = p1+p2;
+            p1 = p2;
+            p2 = ans;
+        }
+        return ans;
+    }
+}
