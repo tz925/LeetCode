b6a2394c95eed2db7f95889aaf319fcdb0869071
tz925 <sai950925@hotmail.com>
2019-09-13 12:55:23 -0400

    Plus One

--- src/com/Thomaszhou/sample/Main.java
+++ src/com/Thomaszhou/sample/Main.java
@@ -7,35 +7,28 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String s = "A man, a plan, a canal: Panama";
-        s = s.replaceAll("[^\\w]", "");
-        System.out.println(s);
-        s = s.replaceAll("/s","");
-        System.out.println(s);
-        int a = 5;
-        int b = a;
-        b = 10;
-        System.out.println(a);
-        System.out.println(b);
-        int[][] image = new int[][]{{1,1,1},{1,1,0},{1,0,1}};
-        image = FloodFill.floodFill(image, 1,1, 2);
-        for(int[] i : image){
-            for(int j : i){
-                System.out.println(j);
-            }
-        }
-        System.out.println("45: " +  5.0/3);
+//        String s = "A man, a plan, a canal: Panama";
+//        s = s.replaceAll("[^\\w]", "");
+//        System.out.println(s);
+//        s = s.replaceAll("/s","");
+//        System.out.println(s);
+//        int a = 5;
+//        int b = a;
+//        b = 10;
+//        System.out.println(a);
+//        System.out.println(b);
+//        int[][] image = new int[][]{{1,1,1},{1,1,0},{1,0,1}};
+//        image = FloodFill.floodFill(image, 1,1, 2);
+//        for(int[] i : image){
+//            for(int j : i){
+//                System.out.println(j);
+//            }
+//        }
+//        System.out.println("45: " +  5.0/3);
 
-        CountAndSay cas = new CountAndSay();
-        System.out.println(cas.countAndSay(1));
-        System.out.println(cas.countAndSay(2));
-        System.out.println(cas.countAndSay(3));
-        System.out.println(cas.countAndSay(4));
-        System.out.println(cas.countAndSay(5));
-        System.out.println(cas.countAndSay(6));
-        System.out.println(cas.countAndSay(7));
-        System.out.println(cas.countAndSay(8));
-        System.out.println(cas.countAndSay(9));
-        System.out.println(cas.countAndSay(10));
+        PlusOne ps = new PlusOne();
+        int[] input = new int[1];
+        input[0] = 9;
+        ps.plusOne(input);
     }
 }
