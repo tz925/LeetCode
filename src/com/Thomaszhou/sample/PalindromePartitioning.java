ac0112f2480d446b3f8235bcd10b8046fea9e2bf
tz925 <sai950925@hotmail.com>
2019-11-15 11:14:39 -0500

    BackTracking DFS palindrome Partitioning

---
+++ src/com/Thomaszhou/sample/PalindromePartitioning.java
@@ -0,0 +1,51 @@
+package com.Thomaszhou.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return all possible palindrome partitioning of s.
+
+Example:
+
+Input: "aab"
+Output:
+[
+  ["aa","b"],
+  ["a","a","b"]
+]
+ */
+public class PalindromePartitioning {
+    List<List<String>> resultLst;
+    ArrayList<String> currLst;
+    public List<List<String>> partition(String s) {
+        resultLst = new ArrayList<List<String>>();
+        currLst = new ArrayList<String>();
+        backTrack(s,0);
+        return resultLst;
+    }
+    public void backTrack(String s, int l){
+        if(currLst.size()>0 //the initial str could be palindrome
+                && l>=s.length()){
+            List<String> r = (List<String>) currLst.clone();
+            resultLst.add(r);
+        }
+        for(int i=l;i<s.length();i++){
+            if(isPalindrome(s,l,i)){
+                currLst.add(s.substring(l,i+1));
+                backTrack(s,i+1);
+                currLst.remove(currLst.size()-1);
+            }
+        }
+    }
+    public boolean isPalindrome(String str, int l, int r){
+        if(l==r) return true;
+        while(l<r){
+            if(str.charAt(l)!=str.charAt(r)) return false;
+            l++;r--;
+        }
+        return true;
+    }
+}
