f6de76652de23434d488ccfa90236034756eec84
tz925 <sai950925@hotmail.com>
2019-12-10 23:13:02 -0500

    Word Search

--- src/com/Thomaszhou/sample/Main.java
+++ src/com/Thomaszhou/sample/Main.java
@@ -29,8 +29,12 @@ public class Main {
 //        UniquePaths up = new UniquePaths();
 //        System.out.println(up.uniquePaths(4,7));
 
-        Search2DMatrixII sm = new Search2DMatrixII();
-        int[][] matrix = new int[][]{{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
-        System.out.println(sm.searchMatrix(matrix, 20));
+//        Search2DMatrixII sm = new Search2DMatrixII();
+//        int[][] matrix = new int[][]{{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+//        System.out.println(sm.searchMatrix(matrix, 20));
+
+        char[][] board = new char[][]{{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        WordSearch ws = new WordSearch();
+        System.out.println(ws.exist(board, "ABCCED"));
     }
 }
