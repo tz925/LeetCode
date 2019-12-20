5633df8a7accb5223fce86c3efa873985d9018eb
tz925 <sai950925@hotmail.com>
2019-12-20 22:25:02 +0800

    Factorial Trailing Zeroes

---
+++ src/com/Thomaszhou/sample/Math/FactorialTrailingZeroes.java
@@ -0,0 +1,31 @@
+package com.Thomaszhou.sample.Math;
+/*
+Given an integer n, return the number of trailing zeroes in n!.
+
+Example 1:
+
+Input: 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+Example 2:
+
+Input: 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+Note: Your solution should be in logarithmic time complexity.
+ */
+public class FactorialTrailingZeroes {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while (n != 0) {
+            int tmp = n / 5;
+            count += tmp;
+            n = tmp;
+        }
+        return count;
+    }
+//     public int trailingZeroes(int n) {
+//         return n==0? 0: n/5+trailingZeroes(n/5);
+//     }
+
+}
