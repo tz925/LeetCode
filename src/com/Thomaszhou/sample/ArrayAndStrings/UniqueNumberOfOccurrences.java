013ef9704f183d42fa7230d48e5ac9f8468e85f0
tz925 <sai950925@hotmail.com>
2020-01-20 18:55:01 -0500

    Unique number of occurence

---
+++ src/com/Thomaszhou/sample/ArrayAndStrings/UniqueNumberOfOccurrences.java
@@ -0,0 +1,36 @@
+package com.Thomaszhou.sample.ArrayAndStrings;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/*
+Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
+
+
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ */
+public class UniqueNumberOfOccurrences {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int a: arr) {
+            map.put(a, map.getOrDefault(a, 0)+1);
+        }
+        Set<Integer> s = new HashSet<>(map.values());
+        return s.size() == map.size();
+    }
+}
